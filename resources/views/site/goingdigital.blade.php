8bbe4a29f321ba43d1396de580658b3e33550692
mrdevil786 <suhailsaeedme@gmail.com>
2024-07-27 01:31:59 +0530

    Minor Changes

--- resources/views/site/goingdigital.blade.php
+++ resources/views/site/goingdigital.blade.php
@@ -37,7 +37,7 @@
                         <li class="slider-item">
 
                             <figure class="img-holder" style="--width: 575; --height: 550;">
-                                <img src="{{ asset('website/assets/images/hero-slide-1.jpg') }}" width="575"
+                                <img src="{{ asset('website/assets/images/services/image-06.png') }}" width="575"
                                     height="550" alt="" class="img-cover">
                             </figure>
 
--- resources/views/site/proservices.blade.php
+++ resources/views/site/proservices.blade.php
@@ -43,7 +43,7 @@
                         <li class="slider-item">
 
                             <figure class="img-holder" style="--width: 575; --height: 550;">
-                                <img src="{{ asset('website/assets/images/hero-slide-1.jpg') }}" width="575"
+                                <img src="{{ asset('website/assets/images/services/image-02.png') }}" width="575"
                                     height="550" alt="" class="img-cover">
                             </figure>
 
