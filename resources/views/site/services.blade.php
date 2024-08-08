4147dbb77fa56e2f4375ee868084621f52283af4
mrdevil786 <suhailsaeedme@gmail.com>
2024-08-09 00:00:50 +0530

    Minor Updates

--- resources/views/site/services.blade.php
+++ resources/views/site/services.blade.php
@@ -4,69 +4,131 @@
 
 @section('website-main-section')
 
-<section class="section hero has-bg-image" aria-label="home"
-    style="background-image: url('{{ asset('website/assets/images/Animated Shape.svg') }}')">
-    <div class="container">
+    <section class="section hero has-bg-image" aria-label="home"
+        style="background-image: url('{{ asset('website/assets/images/Animated Shape.svg') }}')">
+        <div class="container">
 
-        <div class="hero-content">
+            <div class="hero-content">
 
-            <h1 class="h1 hero-title">Our Services</h1>
+                <h1 class="h1 hero-title">Our Services</h1>
 
-            <p class="hero-text">
-                We provide comprehensive business and consultancy services designed to
-                help your organization thrive in today's competitive landscape. Our
-                team of seasoned professionals brings a wealth of experience and
-                expertise to each project, ensuring tailored solutions that meet your
-                unique needs.
-            </p>
-            <p class="hero-text">
-                Explore our range of services below.
-            </p>
+                <p class="hero-text">
+                    We provide comprehensive business and consultancy services designed to
+                    help your organization thrive in today's competitive landscape. Our
+                    team of seasoned professionals brings a wealth of experience and
+                    expertise to each project, ensuring tailored solutions that meet your
+                    unique needs.
+                </p>
+                <p class="hero-text">
+                    Explore our range of services below.
+                </p>
 
-            <div class="btn-wrapper">
-                <a href="{{ url('/contact') }}#contact-form" class="btn btn-outline">Contact Us</a>
+                <div class="btn-wrapper">
+                    <a href="{{ url('/contact') }}#contact-form" class="btn btn-outline">Contact Us</a>
+                </div>
+
+            </div>
+
+            <div class="hero-slider" data-slider>
+
+                <div class="slider-inner">
+                    <ul class="slider-container" data-slider-container>
+
+                        <li class="slider-item">
+                            <figure class="img-holder" style="--width: 575; --height: 550;">
+                                <img src="{{ asset('website/assets/images/services-slide-01.jpeg') }}" width="575"
+                                    height="550" alt="" class="img-cover">
+                            </figure>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <button class="slider-btn prev" aria-label="slide to previous" data-slider-prev>
+                    <ion-icon name="arrow-back"></ion-icon>
+                </button>
+
+                <button class="slider-btn next" aria-label="slide to next" data-slider-next>
+                    <ion-icon name="arrow-forward"></ion-icon>
+                </button>
+
             </div>
 
         </div>
+    </section>
 
-        <div class="hero-slider" data-slider>
+    <!-- Services -->
 
-            <div class="slider-inner">
-                <ul class="slider-container" data-slider-container>
+    <section class="section service" aria-labelledby="service-label">
+        <div class="container">
 
-                    <li class="slider-item">
-                        <figure class="img-holder" style="--width: 575; --height: 550;">
-                            <img src="{{ asset('website/assets/images/services-slide-01.jpeg') }}" width="575"
-                                height="550" alt="" class="img-cover">
-                        </figure>
-                    </li>
+            <h2 class="h2 section-title">What We Do?</h2>
 
-                </ul>
-            </div>
+            <ul class="grid-list">
 
-            <button class="slider-btn prev" aria-label="slide to previous" data-slider-prev>
-                <ion-icon name="arrow-back"></ion-icon>
-            </button>
+                <li>
+                    <a href="{{ url('/consultancyservice') }}">
+                        <div class="service-card">
 
-            <button class="slider-btn next" aria-label="slide to next" data-slider-next>
-                <ion-icon name="arrow-forward"></ion-icon>
-            </button>
+                            <div class="card-image">
+                                <img src="{{ asset('website/assets/images/services/image-01.png') }}"
+                                    alt="Corporate Gifting" style="width: 100%; border-radius: var(--radius-6);">
+                            </div>
 
-        </div>
+                            <h3 class="h4 card-title">Consultancy Service</h3>
 
-    </div>
-</section>
+                            <a href="{{ url('/consultancyservice') }}" class="btn-text">
+                                <span class="span">Learn More</span>
+                                <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                            </a>
 
-<!-- Services -->
+                        </div>
+                    </a>
+                </li>
 
-<section class="section service" aria-labelledby="service-label">
-    <div class="container">
+                <li>
+                    <a href="{{ url('/proservices') }}">
+                        <div class="service-card">
 
-        <p class="section-subtitle" id="service-label">What We Do?</p>
+                            <div class="card-image">
+                                <img src="{{ asset('website/assets/images/services/image-02.png') }}"
+                                    alt="Corporate Gifting" style="width: 100%; border-radius: var(--radius-6);">
+                            </div>
 
-        <ul class="grid-list">
+                            <h3 class="h4 card-title">PRO Services</h3>
 
-            <li>
+                            <a href="{{ url('/proservices') }}" class="btn-text">
+                                <span class="span">Learn More</span>
+                                <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                            </a>
+
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ url('/goingdigital') }}">
+                        <div class="service-card">
+
+                            <div class="card-image">
+                                <img src="{{ asset('website/assets/images/services/image-06.png') }}"
+                                    alt="Corporate Gifting" style="width: 100%; border-radius: var(--radius-6);">
+                            </div>
+
+                            <h3 class="h4 card-title">Going Digital</h3>
+
+                            <a href="{{ url('/goingdigital') }}" class="btn-text">
+                                <span class="span">Learn More</span>
+                                <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                            </a>
+
+                        </div>
+                    </a>
+                </li>
+
+                {{-- ############### --}}
+
+                {{-- <li>
                 <a href="{{ url('/notaryattestation') }}">
                     <div class="service-card">
 
@@ -84,29 +146,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
-                <a href="{{ url('/proservices') }}">
-                    <div class="service-card">
-
-                        <div class="card-image">
-                            <img src="{{ asset('website/assets/images/services/image-02.png') }}"
-                                alt="Corporate Gifting" style="width: 100%; border-radius: var(--radius-6);">
-                        </div>
-
-                        <h3 class="h4 card-title">PRO Services</h3>
-
-                        <a href="{{ url('/proservices') }}" class="btn-text">
-                            <span class="span">Learn More</span>
-                            <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-                        </a>
-
-                    </div>
-                </a>
-            </li>
-
-            <li>
+                {{-- <li>
                 <a href="{{ url('/certificateattestation') }}">
                     <div class="service-card">
 
@@ -124,9 +166,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
+                {{-- <li>
                 <a href="{{ url('/governmentservices') }}">
                     <div class="service-card">
 
@@ -144,9 +186,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
+                {{-- <li>
                 <a href="{{ url('/translationservices') }}">
                     <div class="service-card">
 
@@ -164,29 +206,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
-                <a href="{{ url('/goingdigital') }}">
-                    <div class="service-card">
-
-                        <div class="card-image">
-                            <img src="{{ asset('website/assets/images/services/image-06.png') }}"
-                                alt="Corporate Gifting" style="width: 100%; border-radius: var(--radius-6);">
-                        </div>
-
-                        <h3 class="h4 card-title">Going Digital</h3>
-
-                        <a href="{{ url('/goingdigital') }}" class="btn-text">
-                            <span class="span">Learn More</span>
-                            <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-                        </a>
-
-                    </div>
-                </a>
-            </li>
-
-            <li>
+                {{-- <li>
                 <a href="{{ url('/managementconsulting') }}">
                     <div class="service-card">
 
@@ -204,9 +226,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
+                {{-- <li>
                 <a href="{{ url('/commercialbrokerage') }}">
                     <div class="service-card">
 
@@ -224,9 +246,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
+                {{-- <li>
                 <a href="{{ url('/customizedgift') }}">
                     <div class="service-card">
 
@@ -244,9 +266,9 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-            <li>
+                {{-- <li>
                 <a href="{{ url('/eventmanagement') }}">
                     <div class="service-card">
 
@@ -264,11 +286,11 @@
 
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
-        </ul>
+            </ul>
 
-    </div>
-</section>
+        </div>
+    </section>
 
 @endsection
